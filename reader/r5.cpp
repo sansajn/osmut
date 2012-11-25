0789df430ac65ce2b2061fa99f5543b7a759fc9f
Adam Hlavatovic <sansajn@ymail.com>
2012-11-25 19:54:45 +0100

    + parser with data export and public interface

--- reader/r5.cpp
+++ reader/r5.cpp
@@ -1,10 +1,15 @@
-// r4: on-demand parsing
+// r5
 
 /* recognized symbols: </, />, <?, ?>, text, name
-\note pr tomto type parseru som stratil možnosť overiť valídnosť vstupu 
-pri parsovaní..*/
+\note pr tomto type parseru som stratil možnosť overiť validnosť vstupu 
+pri parsovaní.
+
+\note po pridaní node-u sa rýchlosť parseru znížila 4x (teraz je 2x pomalší 
+ako libxml). Je to spôsobene vektorom (porozmýšlaj nad lazy attrib reading). */
 
 #include <algorithm>
+#include <utility>
+#include <vector>
 #include <cctype>
 #include <fstream>
 #include <string>
@@ -16,6 +21,8 @@ pri parsovaní..*/
 //#define PRINT_OSM_TREE
 
 using std::max;
+using std::pair;
+using std::vector;
 using std::isalpha;
 using std::string;
 using std::ifstream;
@@ -27,41 +34,11 @@ template <int BufferSize = 4096>
 class file_content : boost::noncopyable
 {
 public:
-	file_content(char const * file_name) 
-		: _fin(file_name, ios::in|ios::binary), _bufidx(0) 
-	{
-		alloc();
-		fill();
-	}
-
-	~file_content() 
-	{
-		free();
-		_fin.close();
-	}
-
+	file_content(char const * file_name);
+	~file_content();
 	bool is_open() const {return _fin.is_open();}
-
-	char get() 
-	{
-		if (!eof() && _bufidx == size())
-		{
-			fill();
-			_bufidx = 0;
-		}
-		return _buf[_bufidx++];
-	}
-
-	void getline(string & s) 
-	{
-		char ch = get();
-		while (!eof() && ch != '\n')
-		{
-			s += ch;
-			ch = get();
-		}
-	}
-
+	char get();
+	void getline(string & s);
 	bool eof() const {return !_fin && (_bufidx == size());}
 
 private:
@@ -75,9 +52,94 @@ private:
 	int _bufidx;
 };
 
+template <int BufferSize>
+file_content<BufferSize>::file_content(char const * file_name) 
+	: _fin(file_name, ios::in|ios::binary), _bufidx(0) 
+{
+	alloc();
+	fill();
+}
+
+template <int BufferSize>
+file_content<BufferSize>::~file_content() 
+{
+	free();
+	_fin.close();
+}
+
+template <int BufferSize>
+char file_content<BufferSize>::get() 
+{
+	if (!eof() && _bufidx == size())
+	{
+		fill();
+		_bufidx = 0;
+	}
+	return _buf[_bufidx++];
+}
+
+template <int BufferSize>
+void file_content<BufferSize>::getline(string & s) 
+{
+	char ch = get();
+	while (!eof() && ch != '\n')
+	{
+		s += ch;
+		ch = get();
+	}
+}
+
+
 class parser
 {
 public:
+	typedef pair<string, string> keyval_t;
+
+	enum e_node {start_node, end_node, empty_node};
+
+	parser(char const * file_name);
+	~parser() {}
+	bool read();
+
+	string const & node_name() const {return _curnode.name;}
+	e_node node_type() const {return _curnode.type;}
+	int attribute_count() const {return _curnode.attribs.size();}
+
+	string const & attribute_name(int idx) const {
+		return _curnode.attribs[idx].first;
+	}
+	
+	string const & attribute_value(int idx) const {
+		return _curnode.attribs[idx].second;
+	}
+
+	keyval_t const & attribute(int idx) const {
+		return _curnode.attribs[idx];
+	}
+
+	// error handling
+	bool good() const {return !_error;}
+	boost::format const & error_message() const {return _errmsg;}
+
+private:
+	void node();
+	void start_tag();
+	void end_tag();
+
+	void eat_pi_section();
+
+	void get_sym();
+	void get_complex_sym();
+	void read_name();
+	void read_text();
+	void get_nospace_char();
+	void get_char();
+
+	void error(char const * msg);
+	void error(boost::format const & msg);
+	string offset();
+
+private:
 	enum e_symbol
 	{
 		tag_opener = 1,
@@ -92,57 +154,16 @@ public:
 		eof
 	};
 
-	parser(char const * file_name);
-	~parser() {}
-
-	bool read() 
+	struct node_data
 	{
-		node();
-		get_sym();
-		return good() && !_osm.eof() && sym != eof;
-	}
+		string name;
+		e_node type;
+		vector<keyval_t> attribs;  // spôsobuje 4-nasobné spomalenie parsingu
+	};
 
-	void node() 
-	{
-		if (sym == tag_opener)
-			start_tag();
-		else if (sym == end_tag_opener)
-			end_tag();
-		else
-			error("start-tag opener '<', or end-tag opener '</' expected");
-	}
+	node_data _curnode;
 
-	void eat_pi_section()
-	{
-		do {
-			get_sym();
-		} while (sym != pi_closer);
-
-		get_sym();
-	}
-
-	// error handling
-	bool good() const {return !_error;}
-	boost::format const & error_message() const {return _errmsg;}
-
-private:
-	void __parse();
-	void __node();
-	void start_tag();
-	void end_tag();
-
-	void get_sym();
-	void get_complex_sym();
-	void read_name();
-	void read_text();
-	void get_nospace_char();
-	void get_char();
-
-	void error(char const * msg);
-	void error(boost::format const & msg);
-	string offset();
-
-private:
+	// low-level-parser
 	e_symbol sym;
 	char ch;
 	char next_ch;
@@ -155,8 +176,11 @@ private:
 	bool _error;
 	boost::format _errmsg;
 
+	// debug
 	int tabs;
-};
+};  // parser
+
+
 
 
 void parse(parser & p);
@@ -183,7 +207,18 @@ int main(int argc, char * argv[])
 void parse(parser & p)
 {
 	while (p.read())
+	{
+		if (p.node_type() == parser::start_node)
+		{
+			cout << "node:" << p.node_name() << "\n";
+			for (int i = 0; i < p.attribute_count(); ++i)
+			{
+				cout << "  k:" << p.attribute_name() << ", v:"
+					<< p.attribute_value() << "\n";
+			}
+		}
 		continue;
+	}
 }
 
 parser::parser(char const * file_name)
@@ -204,44 +239,30 @@ parser::parser(char const * file_name)
 		eat_pi_section();
 }
 
-void parser::__parse()
+bool parser::read()
 {
+	node();
 	get_sym();
-
-	if (sym == pi_opener)  // ignore pi section
-	{
-		do {
-			get_sym();
-		} while (sym != pi_closer);
-
-		get_sym();
-	}
-
-	__node();
+	return good() && !_osm.eof() && sym != eof;
 }
 
-void parser::__node()
+void parser::node()
 {
 	if (sym == tag_opener)
 		start_tag();
-	else
-		error("tag opener ('<') expected");
-
-	if (sym == empty_tag_closer)
-		return;
-
-	get_sym();
-
-	while (sym == tag_opener)
-	{
-		node();
-		get_sym();
-	}
-
-	if (sym == end_tag_opener)
+	else if (sym == end_tag_opener)
 		end_tag();
 	else
-		error("new node, or end-tag opener '</' expected");
+		error("start-tag opener '<', or end-tag opener '</' expected");
+}
+
+void parser::eat_pi_section()
+{
+	do {
+		get_sym();
+	} while (sym != pi_closer);
+
+	get_sym();
 }
 
 void parser::start_tag()
@@ -255,6 +276,10 @@ void parser::start_tag()
 	get_sym();
 	if (sym != name)
 		error("start-tag name expected");
+
+	_curnode.name = name_buf;
+	
+	_curnode.attribs.clear();
 	
 	// (name=valaue)*
 	get_sym();
@@ -268,6 +293,8 @@ void parser::start_tag()
 		if (sym != text)
 			error("value expected");
 
+		_curnode.attribs.push_back(make_pair(name_buf, text_buf));
+
 #ifdef PRINT_OSM_TREE
 		tabs += 1;
 		cout << offset() 
@@ -279,7 +306,11 @@ void parser::start_tag()
 	}
 
 	// end_tag
-	if (sym != tag_closer && sym != empty_tag_closer)
+	if (sym == tag_closer)
+		_curnode.type = start_node;
+	else if (sym == empty_tag_closer)
+		_curnode.type = empty_node;
+	else
 		error("tag closer ('>' or '/>') expected.");
 
 #ifdef PRINT_OSM_TREE
@@ -307,6 +338,9 @@ void parser::end_tag()
 	get_sym();
 	if (sym != tag_closer)
 		error("tag closer expected");
+
+	_curnode.name = name_buf;
+	_curnode.type = end_node;
 }
 
 void parser::get_sym()
